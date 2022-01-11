f4b0326b82ea05643884a7e4ecefb4440d48c7af
Wownicehat <34525936+Wownicehat@users.noreply.github.com>
2022-01-12 00:13:56 +0100

    Updated meta

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 
+## 0.0.2
+
+- Updated package.json
+- Added logo
+- Updated markdown files
+
 ## 0.0.1
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 
 import * as vscode from 'vscode';
 
+
+
 export function activate(context: vscode.ExtensionContext) {
 	
 	context.subscriptions.push(
