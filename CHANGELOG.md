66bd38e5a50905c50cdcddc97571dbb9222d5442
GauthierEstJoly <joly.gauthier76@gmail.com>
2022-03-17 21:24:09 +0100

    Added CR support and option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Change Log
 
+## 0.0.3
+
+- Added option to remove \r
+- Fixed bug with \r
+- Fixed bug where last caracter of some lines where getting removed
+- Fixed bad code
+
 ## 0.0.2
 
 - Updated package.json
--- src/extension.ts
+++ src/extension.ts
@@ -9,17 +9,33 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerTextEditorCommand("paste-as-multiline-string.pasteAsMultilineString", async editor =>
 		{
 			let text = await vscode.env.clipboard.readText();
+
+			let deleteCR = vscode.workspace.getConfiguration('paste-as-multiline-string').get("deleteCarriageReturn");
+
+			if (deleteCR === true) {
+				text = text.replace(/\r/g, "");
+			} else {
+				text = text.replace(/\r/g, "\\r");
+			}
+
+			// Replace some special caracters
 			text = text.replace(/\t/g, "\\t");
 			text = text.replace(/\"/g, "\\\"");
 
+
 			let lines = text.split('\n');
 			let newText = '';
+
 			for (let i = 0; i < lines.length; i++) {
-				let actuaLine = lines[i].substring(0, lines[i].length - 1);
-				newText += '\"' + actuaLine + `${i !== lines[i].length ? "\\n\"" : "\""}`;
+
+				let actuaLine = lines[i];
+
+				newText += '\"' + actuaLine;
 
 				if (i < lines.length - 1) {
-					newText += ' +\n';
+					newText += '\\n\" +\n';
+				} else {
+					newText += '\"';
 				}
 			}
 
